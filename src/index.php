3ff9ec37aa15bb5bc47c3704382986b621e8eba4
Weimil <paubergol@gmail.com>
2022-09-23 11:41:22 +0200

    Algo de trabajo en el inicio

---
+++ src/index.php
@@ -0,0 +1,126 @@
+<?php ?>
+<html>
+
+<head>
+  <title>Inicio</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="/src/code/js/wScript.js"></script>
+  <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="/src/code/css/wStyles.css">
+  <link rel="stylesheet" href="/src/code/css/wStyles.css">
+</head>
+
+<body>
+
+  <div id="video">
+    <video controls class="video">
+      <source src="/src/assets/videos/movie.mp4" type="video/mp4">
+    </video>
+  </div>
+
+
+  <div id="presentation" class="container text-center">
+    <div class="row">
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div id="carrousel">
+    <div class="container">
+      <div class="title col-md-12 text-center"> Waterwheel Owl Carousel 2</div>
+      <div class="col-md-12">
+        <div class="owl-carousel owl-theme">
+          <img src="https://picsum.photos/800/450/?random&1">
+          <img src="https://picsum.photos/800/450/?random&2">
+          <img src="https://picsum.photos/800/450/?random&3">
+          <img src="https://picsum.photos/800/450/?random&4">
+          <img src="https://picsum.photos/800/450/?random&5">
+          <img src="https://picsum.photos/800/450/?random&6">
+          <img src="https://picsum.photos/800/450/?random&7">
+          <img src="https://picsum.photos/800/450/?random&8">
+        </div>
+      </div>
+      <div class="text-center">
+        <div class="status"></div>
+      </div>
+    </div>
+  </div> -->
+
+  <div id="contact">
+    <div class="container text-center">
+      <div class="container text-center">
+        <button type="button" data-bs-toggle="collapse" data-bs-target="#contactForm" aria-controls="contactForm"
+          aria-expanded="false" aria-label="Toggle contactForm">
+          Contact Us
+        </button>
+      </div>
+    </div>
+    <div class="collapse container text-center" id="contactForm">
+      <div class="p-4 bg-dark">
+        <h5 class="text-white h4">Collapsed content</h5>
+        <span class="text-muted">Toggleable via the navbar brand.</span>
+      </div>
+    </div>
+  </div>
+
+  <div id="presentation" class="container text-center">
+    <div class="row">
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+      <div class="col tmp">
+        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptas praesentium, repellat
+          blanditiis, molestias dolorem modi, dolore nulla nihil sequi voluptate dolores quidem ut quaerat
+          odit. Nesciunt facere dolores non?</p>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
