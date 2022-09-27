2d7df920aa9c6dfb7804b826491445c2ea45af2a
Weimil <paubergol@gmail.com>
2022-09-27 13:47:18 +0200

    Para que lo vea Emilio

--- src/code/js/wScript.js
+++ src/code/js/wScript.js
@@ -1,3 +1,23 @@
+function coverFlowEfx(e) {
+    if ($(".owl-dots")) {
+        $(".owl-dots").remove();
+    }
+
+    idx = e.item.index;
+    
+    $(".owl-item.big").removeClass("big");
+    $(".owl-item.medium").removeClass("medium");
+    $(".owl-item.mdright").removeClass("mdright");
+    $(".owl-item.mdleft").removeClass("mdleft");
+    $(".owl-item.smallRight").removeClass("smallRight");
+    $(".owl-item.smallLeft").removeClass("smallLeft");
+    $(".owl-item").eq(idx - 1).addClass("medium mdleft");
+    $(".owl-item").eq(idx).addClass("big");
+    $(".owl-item").eq(idx + 1).addClass("medium mdright");
+    $(".owl-item").eq(idx + 2).addClass("smallRight");
+    $(".owl-item").eq(idx - 2).addClass("smallLeft");
+}
+
 $(".owl-carousel").owlCarousel({
     center: true,
     loop: true,
@@ -22,22 +42,4 @@ $(".owl-carousel").owlCarousel({
     onTranslate: coverFlowEfx
 });
 
-function coverFlowEfx(e) {
-    if ($(".owl-dots")) {
-        $(".owl-dots").remove();
-    }
-
-    idx = e.item.index;
-    
-    $(".owl-item.big").removeClass("big");
-    $(".owl-item.medium").removeClass("medium");
-    $(".owl-item.mdright").removeClass("mdright");
-    $(".owl-item.mdleft").removeClass("mdleft");
-    $(".owl-item.smallRight").removeClass("smallRight");
-    $(".owl-item.smallLeft").removeClass("smallLeft");
-    $(".owl-item").eq(idx - 1).addClass("medium mdleft");
-    $(".owl-item").eq(idx).addClass("big");
-    $(".owl-item").eq(idx + 1).addClass("medium mdright");
-    $(".owl-item").eq(idx + 2).addClass("smallRight");
-    $(".owl-item").eq(idx - 2).addClass("smallLeft");
-}
+alert("aaaaaaaaa");
