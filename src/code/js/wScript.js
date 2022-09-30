2ebb3373ca3ca4fcd658624f3a07ccbd40fedf31
Weimil <paubergol@gmail.com>
2022-09-30 12:36:08 +0200

    WIP

--- src/code/js/wScript.js
+++ src/code/js/wScript.js
@@ -1,10 +1,42 @@
 function toggleExtraInfoSelector(obj) {
     let selectors = document.getElementsByClassName("children-selector");
-    selectors.forEach(element => addClass(element, "display-none"));
+    for (let i = 0; i < selectors.length; i++) {
+        addClass(selectors[i], "display-none")
+    }
+    // 
+    let selectors2 = document.getElementsByClassName("info-pannel");
+    for (let i = 0; i < selectors2.length; i++) {
+        addClass(selectors2[i], "display-none")
+    }
+    if (obj.value === "none") return;
     removeClass(document.getElementById(obj.value), "display-none")
 }
 
 function toggleInfoPanel(obj) {
-
+    let selectors2 = document.getElementsByClassName("info-pannel");
+    for (let i = 0; i < selectors2.length; i++) {
+        addClass(selectors2[i], "display-none")
+    }
+    if (obj.value === "none") return;
+    removeClass(document.getElementById(obj.value), "display-none")
 }
 
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+
+function removeClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
--- src/index.php
+++ src/index.php
@@ -4,21 +4,20 @@
   <title>Inicio</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="/src/code/js/wScript.js"></script>
-  <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
-  <link rel="stylesheet" href="/src/code/css/wStyles.css">
-
+  <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+  
+  <script src="code/js/wScript.js"></script>
+  <link rel="stylesheet" href="code/css/wStyles.css">
 </head>
 
 <body>
 
   <div id="video">
     <video controls class="video">
-      <source src="/src/assets/videos/movie.mp4" type="video/mp4">
+      <source src="assets/videos/movie.mp4" type="video/mp4">
     </video>
   </div>
-
 
   <div id="presentation" class="container text-center">
     <div class="row">
@@ -51,12 +50,6 @@
     </div>
   </div>
 
-  <pre>
-    https://codepen.io/weimil/pen/KKRvJdL
-    https://codepen.io/weimil/pen/BaxdOVY
-    https://codepen.io/dreamer22001/pen/GBzbRV
-  </pre>
-
   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -80,16 +73,71 @@
   </div>
 
   <div id="contact">
+  <div id="contact">
     <div class="container text-center">
-      <div class="container text-center">
-        <button type="button" data-bs-toggle="collapse" data-bs-target="#contactForm" aria-controls="contactForm" aria-expanded="false" aria-label="Toggle contactForm">
-          Contact Us
-        </button>
-      </div>
+      <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false"
+        aria-controls="collapseExample">
+        Contact Us
+      </a>
     </div>
-    <div class="collapse container text-center" id="contactForm">
-      <div class="p-4 bg-dark">
-        <form>
+
+    <div class="collapse container text-center" id="collapseExample">
+      <div class="row">
+        <div class="col-4 bg-danger p-1">
+          <div class="m-1 p-1 bg-success">
+            <div class="m-1">
+              <select class="form-select" id="main-selector" onchange="toggleExtraInfoSelector(this)">
+                <option value="none" selected>Open this select menu</option>
+                <option value="selector-courses-info">Couses</option>
+                <option value="selector-general-info">Genral</option>
+              </select>
+
+              <select class="form-select display-none children-selector" id="selector-courses-info" onchange="toggleInfoPanel(this)">
+                <option value="none" selected>Open this select menu</option>
+                <option value="smr-info">SMR</option>
+                <option value="dam-info">DAM</option>
+                <option value="daw-info">DAW</option>
+                <option value="asir-info">ASIR</option>
+              </select>
+
+              <select class="form-select display-none children-selector" id="selector-general-info" onchange="toggleInfoPanel(this)">
+                <option value="none" selected>Open this select menu</option>
+                <option value="location">Ubicación</option>
+                <option value="patata">Patata</option>
+                <option value="tomate">Tomate</option>
+                <option value="cebolla">Cebolla</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-8 bg-danger p-1">
+          <div class="m-1 bg-success" id="test">
+            <div class="display-none info-pannel" id="smr-info">
+              [SMR] Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus adipisci doloribus nesciunt
+              sapiente labore accusantium illo. Quibusdam dicta unde, sapiente tenetur, cumque praesentium sunt
+              molestias illum odit nulla vel numquam!
+            </div>
+            <div class="display-none info-pannel" id="dam-info">
+              [DAM] Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus adipisci doloribus nesciunt
+              sapiente labore accusantium illo. Quibusdam dicta unde, sapiente tenetur, cumque praesentium sunt
+              molestias illum odit nulla vel numquam!
+            </div>
+            <div class="display-none info-pannel" id="daw-info">
+              [DAW] Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus adipisci doloribus nesciunt
+              sapiente labore accusantium illo. Quibusdam dicta unde, sapiente tenetur, cumque praesentium sunt
+              molestias illum odit nulla vel numquam!
+            </div>
+            <div class="display-none info-pannel" id="asir-info">
+              [ASIR] Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus adipisci doloribus nesciunt
+              sapiente labore accusantium illo. Quibusdam dicta unde, sapiente tenetur, cumque praesentium sunt
+              molestias illum odit nulla vel numquam!
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- <form>
           <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -104,9 +152,9 @@
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
+        </form> -->
     </div>
+  </div>
   </div>
 
   <div id="presentation" class="container text-center">
