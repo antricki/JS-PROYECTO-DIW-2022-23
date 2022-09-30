68ea9b9d31c9e3957af6e9a3a9ee647f2108e517
CarOrLa03 <carmenorla03@gmail.com>
2022-09-30 21:13:41 +0200

    tramitar_pedido a medias

--- src/store/politicas.js
+++ src/store/politicas.js
@@ -12,3 +12,19 @@ enviar.addEventListener('click', ()=>{
     textArea.classList.add('div_hidden');
     textThanks.classList.remove('div_hidden');
 })
+// js para tramitar pedido, muestra una alerta tlf
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Para que se pongan en contacto contigo, la empresa de reparto, en caso de necesidad.', 'success')
+  })
+}
