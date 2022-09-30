16230df55db056a1d650064f8a0f14ae38dde7a4
Emilio <emilioblasco@icloud.com>
2022-09-30 20:19:59 +0200

    solucionando problemas galeria 1

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -1,69 +1,80 @@
 //DECLARACIÓN DE CONSTANTES
 
 //Contenedor con todas las imágenes
-const images = document.getElementById('images');
+const images = document.getElementById("images");
 
 //Identificamos los botones
-const dLeft = document.getElementById('dotLeft');
-const dRight = document.getElementById('dotRight');
-
+const dLeft = document.getElementById("dotLeft");
+const dRight = document.getElementById("dotRight");
 
 //EVENTOS
 
+//Función antirrebote
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
 //Evento botón izquierdo
-dLeft.addEventListener('click', (e) => {
+dLeft.addEventListener(
+  "click",
+  debounce(() => {
     //Identificamos el primer elemento
-    const imgFirst = document.querySelectorAll('.img')[0];
-    
+    const imgFirst = document.querySelectorAll(".img")[0];
+
     //Aplicamos la animación
-    images.style.transition = "transform 1s linear";
+    images.style.transition = "transform .5s linear";
     images.style.transform = "translateX(-240px)";
-    actualizarEstilos('left');     
-    
+    actualizarEstilos("left");
+
     //Colocamos la primera imagen en última posición
-    setTimeout(() =>{    
-        images.insertAdjacentElement('beforeend', imgFirst);    
-        images.style.transition = "none";        
-        images.style.transform = "translateX(0px)";
-    },1000);
-})
+    setTimeout(() => {
+      images.insertAdjacentElement("beforeend", imgFirst);
+      images.style.transition = "none";
+      images.style.transform = "translateX(0px)";
+    }, 500);
+  }, 3000)
+);
 
 //Evento botón derecho
-dRight.addEventListener('click', (e) => {
-    //Identificamos el último elemento
-    const img = document.querySelectorAll('.img');
-    const imgLast = img[img.length - 1];
+dRight.addEventListener("click", debounce(() => {
+  //Identificamos el último elemento
+  const img = document.querySelectorAll(".img");
+  const imgLast = img[img.length - 1];
 
-    //Aplicamos la animación
-    images.style.transition = "transform 1s linear";
-    images.style.transform = "translateX(240px)";
-    actualizarEstilos("right");  
-    
-    
-    //Colocamos la útlima imagen en primera posición
-    setTimeout(() =>{  
-        images.insertAdjacentElement('afterbegin', imgLast);      
-        images.style.transition = "none";        
-        images.style.transform = "translateX(0px)";
-        
-    },1000);
-})
+  //Aplicamos la animación
+  images.style.transition = "transform .5s linear";
+  images.style.transform = "translateX(240px)";
+  actualizarEstilos("right");
+
+  //Colocamos la útlima imagen en primera posición
+  setTimeout(() => {
+    images.insertAdjacentElement("afterbegin", imgLast);
+    images.style.transition = "none";
+    images.style.transform = "translateX(0px)";
+  }, 500);
+}, 300)
+);
 
 const actualizarEstilos = (direcction) => {
-    const img = document.querySelectorAll('.img');
-    const n = (direcction === 'right') ? 
-        (Math.round(img.length / 2)) - 2 : 
-        (Math.round(img.length / 2));
-    const big = img[n];
-    const normal = [img[(n-1)], img[(n+1)]];
-    // const small = [img[(n-2)], img[(n+2)]];
-    img.forEach((image) => {
-        image.classList.remove('img--big', 'img--normal');
-    })
-    big.classList.add('img--big');
-    normal.forEach((image) => {
-        image.classList.add('img--normal');
-    })
-
-
-}
+  const img = document.querySelectorAll(".img");
+  const n =
+    direcction === "right"
+      ? Math.round(img.length / 2) - 2
+      : Math.round(img.length / 2);
+  const big = img[n];
+  const normal = [img[n - 1], img[n + 1]];
+  // const small = [img[(n-2)], img[(n+2)]];
+  img.forEach((image) => {
+    image.classList.remove("img--big", "img--normal");
+  });
+  big.classList.add("img--big");
+  normal.forEach((image) => {
+    image.classList.add("img--normal");
+  });
+};
