e854bd1bb30d2f93b09a58a5d53f297fdf8a53c0
CarOrLa03 <carmenorla03@gmail.com>
2022-10-01 21:03:06 +0200

    continuacion tramitar pedido

--- src/store/politicas.js
+++ src/store/politicas.js
@@ -12,19 +12,10 @@ enviar.addEventListener('click', ()=>{
     textArea.classList.add('div_hidden');
     textThanks.classList.remove('div_hidden');
 })
-// js para tramitar pedido, muestra una alerta tlf
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-var alertTrigger = document.getElementById('liveAlertBtn')
 
-function alert(message, type) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+// js para tooltip tlf
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
-  alertPlaceholder.append(wrapper)
-}
-
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', function () {
-    alert('Para que se pongan en contacto contigo, la empresa de reparto, en caso de necesidad.', 'success')
-  })
-}
