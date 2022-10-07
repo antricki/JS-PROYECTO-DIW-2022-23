de56bed77a04af7b9a0f62b7eda341a1a348bb10
alexdracma <alexdracma@gmail.com>
2022-10-07 13:06:59 +0200

    Arreglado margen del footer

--- src/code/php/footer.php
+++ src/code/php/footer.php
@@ -8,7 +8,8 @@
                 <a class="allfooter" href="mailto:dptoinformatica@fpcheste.com"><img src="<?php echo $dir . 'assets/icons/mail.svg'?>" alt=""></a>
                 <a class="allfooter" href="https://github.com/antricki/PROYECTO-DIW-2022-23/"><img src="<?php echo $dir . 'assets/icons/github.svg'?>" alt=""></a>
             </div>
-            <div class="col-md-8 pags-allfooter mt-sm-5">
+            <div id="sm-margin-allfooter" class="mt-5 d-block d-md-none"></div>
+            <div class="col-md-8 pags-allfooter">
                 <h3 class="mb-3 allfooter">Páginas de interés</h3>
                 <a class="allfooter" href="https://foremp.edu.gva.es/"><img src="../../assets/icons/saofct.png" alt=""></a>
                 <a class="allfooter" href="https://familia.edu.gva.es/wf-front/myitaca/login_wf?idioma=C"><img src="<?php echo $dir . 'assets/icons/itaca.png'?>" alt=""></a>
