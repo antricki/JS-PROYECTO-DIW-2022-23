6852bd00670d97f62a7a2de212b9735d53c2d58c
Pablo <pabloruizalfaro@gmail.com>
2022-11-24 09:41:41 +0100

    jueves

--- src/login/login.php
+++ src/login/login.php
@@ -46,9 +46,7 @@
                 </div>
 
                 <div class="tempText">
-                  <span class="textoTemporal">
-                    Correo electrónico
-                  </span>
+                  <span class="textoTemporal"> Correo electrónico </span>
                 </div>
               </div>
 
@@ -92,13 +90,69 @@
                 >
               </div>
               <div class="col-7">
-                <a href="#" class="enlace">¿Olvidaste la contraseña?</a>
+                <a href="#" class="enlace" id="eRecover"
+                  >¿Olvidaste la contraseña?</a
+                >
               </div>
             </div>
 
             <div class="form-group">
-              <button type="button" class="btn1 mt-3 mb-5" name="logInBtn">Login</button>
+              <button type="button" class="btn1 mt-3 mb-5" name="logInBtn">
+                Login
+              </button>
             </div>
+
+            <br />
+
+            <div class="secundario">
+              <span>¿Todavía no estás registrado?</span>
+              <a href="#" class="enlace" onclick="showRegis()" id="bSignup-show"
+                >Regístrate aquí</a
+              >
+            </div>
+          </form>
+        </div>
+
+        <div class="form-box recover-form" id="recover-form">
+          <div class="form-title">
+            <h2 class="fw-bold mb-3">Recupera tu contaseña</h2>
+          </div>
+          <form class="col-12" id="formulario">
+            <div class="form-group" id="user-group">
+              <div
+                class="userPlaceholderRecover dummytext"
+                id="userPlaceholderRecover"
+              >
+                <div class="tempIcon">
+                  <img
+                    src="../assets/icons/circle-user-solid.svg"
+                    alt=""
+                    class="userIcon"
+                  />
+                </div>
+
+                <div class="tempText">
+                  <span class="textoTemporal"> Correo electrónico </span>
+                </div>
+              </div>
+
+              <input
+                id="emailRecover"
+                type="email"
+                placeholder=""
+                class="form-control my-3 p-2"
+              />
+            </div>
+
+            <div class="form-group">
+              <button type="button" class="btn1 mt-3 mb-5" name="logInBtn">
+                Recuperar
+              </button>
+            </div>
+            <span>¿Ya tienes cuenta?</span>
+            <a href="#" class="enlace" onclick="showLogin()" id="bLogin-show"
+              >Cónectate aquí</a
+            >
 
             <br />
 
@@ -218,7 +272,6 @@
                 <div class="tempText">
                   <span class="textoTemporal"> Contraseña </span>
                 </div>
-                
               </div>
               <div class="campoValido campoValidoPassRegis dummytext">
                 <div class="tempIcon">
@@ -248,8 +301,50 @@
                 name="passRegis"
               />
             </div>
+            <div class="form-group boxForm" id="pass-group">
+              <div class="passPlaceholderRegis2 dummytext">
+                <div class="tempIcon">
+                  <img
+                    src="../assets/icons/lock-solid.svg"
+                    alt=""
+                    class="passIcon"
+                  />
+                </div>
+
+                <div class="tempText">
+                  <span class="textoTemporal"> Confirmar Contraseña </span>
+                </div>
+              </div>
+              <div class="campoValido campoValidoPassRegis dummytext">
+                <div class="tempIcon">
+                  <img
+                    src="../assets/icons/check-solid.svg"
+                    alt=""
+                    class="iconValido"
+                  />
+                </div>
+              </div>
+              <div class="campoInValido campoInValidoPassRegis dummytext">
+                <div class="tempIcon">
+                  <img
+                    src="../assets/icons/x-solid.svg"
+                    alt=""
+                    class="iconInValido"
+                  />
+                </div>
+              </div>
+              <input
+                id="passRegis2"
+                type="password"
+                placeholder=""
+                class="form-control my-3 p-2"
+                name="passRegis2"
+              />
+            </div>
             <div class="form-group">
-              <button type="button" class="btn1 mt-3 mb-5" name="signUpBtn">Sign up</button>
+              <button type="button" class="btn1 mt-3 mb-5" name="signUpBtn">
+                Sign up
+              </button>
             </div>
 
             <span>¿Ya tienes cuenta?</span>
@@ -272,7 +367,6 @@
     <br />
 
     <script src="login.js"></script>
- 
 
     <?php
   include "../code/php/footer.php";
